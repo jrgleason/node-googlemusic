e9c9497fe96cae302a26445478fb29f9a994c651
Your Name <jackiegleason@gmail.com>
2012-07-29 21:56:25 -0400

    Oops removed this

--- test/getCookieTest.js
+++ test/getCookieTest.js
@@ -4,24 +4,35 @@ var assert = require('assert')
 
 describe('cookie', function(){
   
-  describe('#getUsername()', function(){
-    it('should return  user', function(){
-      cookie.setUsername("user")
-      assert.equal('user', cookie.getUsername());
-    })
-  })
   describe('#execute()',function(){
     it('Should return a cookie', function(){
-      cookie.setUsername("user");
-      cookie.setPassword("password");
       nock('https://accounts.google.com')
                 .get('/ServiceLogin?service=sj')
-                .reply(200, '<input type="hidden" name="dsh" id="dsh" value="-4906219821262685280"><input type="hidden" name="GALX" value="eSdl2XQF6dk">');
+                .reply(200, '<input type="hidden" name="dsh" id="dsh" value="-4906219821262685280"><input type="hidden" name="GALX" value="eSdl2XQF6dk">', {'set-cookie': 'MMMMM Cooookie'} );
       cookie.execute(function(err,dsh,galx,cookie){
         assert.equal('-4906219821262685280',dsh,"DSH is invalid")
         assert.equal('eSdl2XQF6dk',galx,"GALX is Invalid")
+        assert.equal('MMMMM Cooookie',cookie,"Cookie is Invalid")
       })
     })
   })
+  describe('#execute()',function(){
+    it('Testing Integration', function(){
+      try{
+        cookie.execute(function(err,dsh,galx,cookie){
+          console.log(dsh+"|"+galx+"|"+cookie)
+          //assert.notEqual(undefined,dsh,"DSH is invalid")
+          assert.equal('sd','ds',"DSH is invalid")
+          assert.notEqual(undefined,galx,"GALX is Invalid")
+          assert.notEqual(undefined,cookie,"Cookie is Invalid")
+        })
+      }
+      catch(err){
+        console.log("Error:"+err)
+      }
+      console.log("Ending")
+    })
+  })
+
 
 })
